ef01bf964b604282c8eeee8b9f8cf39095c2d35b
IanMin1022 <ianjasonmin@gmail.com>
2024-03-06 11:32:49 +0900

    Fixed path searching method

--- converter.py
+++ converter.py
@@ -15,14 +15,13 @@ import csv
 
 from dataset import Dataset
 
+
 class labelme2coco:
     def __init__(self):
         print("ready to convert labelme data 2 coco")
 
     def get_coco_from_labelme_folder(
-        self,
-        labelme_folder: str,
-        coco_category_list: List = None
+        self, labelme_folder: str, coco_category_list: List = None
     ) -> Coco:
         """
         Args:
@@ -49,10 +48,10 @@ class labelme2coco:
         ):
             try:
                 data = load_json(json_path)
-                
+
                 # get image size
                 image_path = str(Path(labelme_folder) / data["imagePath"])
-                
+
                 # use the image sizes provided by labelme (they already account for
                 # things such as EXIF orientation)
                 width = data["imageWidth"]
@@ -65,7 +64,7 @@ class labelme2coco:
                 for shape in data["shapes"]:
                     # set category name and id
                     category_name = shape["label"]
-                    
+
                     category_id = None
                     for (
                         coco_category_id,
@@ -81,7 +80,7 @@ class labelme2coco:
                             CocoCategory(id=category_id, name=category_name)
                         )
                         category_ind += 1
-    
+
                     # circles and lines to segmentation
                     if shape["shape_type"] == "circle":
                         (cx, cy), (x1, y1) = shape["points"]
@@ -106,7 +105,7 @@ class labelme2coco:
                             y1 + 1e-3,
                         ]
                         shape["shape_type"] = "polygon"
-    
+
                     # parse bbox/segmentation
                     if shape["shape_type"] == "rectangle":
                         x1 = shape["points"][0][0]
@@ -133,15 +132,10 @@ class labelme2coco:
                 coco.add_image(coco_image)
             except Exception as e:
                 continue
-        
+
         return coco
 
-    def convert(
-        self,
-        input_dir:str,
-        output_dir: str,
-        train_split_rate: float = 1
-    ):        
+    def convert(self, input_dir: str, output_dir: str, train_split_rate: float = 1):
         """
         Args:
             input_dir: folder that contains labelme annotations and image files
@@ -154,10 +148,13 @@ class labelme2coco:
         image_dir = None
 
         for dirpath, dirnames, filenames in os.walk(input_dir):
-            if any(filename.endswith('.json') for filename in filenames) and any(filename.lower().endswith(('.png', '.jpg', '.jpeg')) for filename in filenames):
+            if any(filename.endswith(".json") for filename in filenames) and any(
+                filename.lower().endswith((".png", ".jpg", ".jpeg"))
+                for filename in filenames
+            ):
                 input_dir = dirpath
                 break
-                
+
         coco = self.get_coco_from_labelme_folder(labelme_folder=input_dir)
 
         if 0 < train_split_rate < 1:
@@ -171,7 +168,8 @@ class labelme2coco:
         else:
             save_path = str(Path(output_dir) / "dataset.json")
             save_json(coco.json, save_path)
-            
+
+
 class labelme2yolo:
     def __init__(self, dataset=None):
         print("ready to convert labelme data 2 yolo")
@@ -227,117 +225,126 @@ class labelme2yolo:
 
         # Write back to the original dataframe
         df["cat_id"] = df_copy["cat_id"]
-        
+
     def json2df(self, annotations_json, path, type=""):
-        image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]        
+        image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
         image_dir = None
-        
-        for file_path in glob.glob(os.path.join(path, '*')):
+
+        for file_path in glob.glob(path):
             if os.path.isdir(file_path):
-                for sub_path in glob.glob(os.path.join(file_path, '*')):
+                for sub_path in glob.glob(os.path.join(file_path, "*")):
                     if "images/train" in sub_path:
                         break
                     elif "images/val" in sub_path:
                         break
-                    elif os.path.isfile(sub_path) and any(sub_path.lower().endswith(ext) for ext in image_extensions):
+                    elif os.path.isfile(sub_path) and any(
+                        sub_path.lower().endswith(ext) for ext in image_extensions
+                    ):
                         image_dir = file_path
                         break
-                    
+
         if type == "val":
             add_path = "/val"
         elif type == "train":
             add_path = "/train"
         else:
             add_path = ""
-                    
+
         images = pd.json_normalize(annotations_json["images"])
         images.columns = "img_" + images.columns
         try:
             images["img_folder"]
         except:
             images["img_folder"] = ""
-            # print(images)
-                            
-        astype_dict = {"img_width": "int64", "img_height": "int64", "img_depth": "int64"}
+
+        astype_dict = {
+            "img_width": "int64",
+            "img_height": "int64",
+            "img_depth": "int64",
+        }
         astype_keys = list(astype_dict.keys())
-        
+
         for element in astype_keys:
             if element not in images.columns:
                 astype_dict.pop(element)
-        # print(astype_dict)
-        
+
         images = images.astype(astype_dict)
-    
+
         annotations = pd.json_normalize(annotations_json["annotations"])
         annotations.columns = "ann_" + annotations.columns
-    
+
         categories = pd.json_normalize(annotations_json["categories"])
         categories.columns = "cat_" + categories.columns
-    
+
         # Converting this to string resolves issue #23
         categories.cat_id = categories.cat_id.astype(str)
-                
+
         # Converting this to string resolves issue #23
         df = annotations
         df.ann_category_id = df.ann_category_id.astype(str)
-    
-        df[["ann_bbox_xmin", "ann_bbox_ymin", "ann_bbox_width", "ann_bbox_height"]] = pd.DataFrame(
-            df.ann_bbox.tolist(), index=df.index
+
+        df[["ann_bbox_xmin", "ann_bbox_ymin", "ann_bbox_width", "ann_bbox_height"]] = (
+            pd.DataFrame(df.ann_bbox.tolist(), index=df.index)
         )
         df.insert(8, "ann_bbox_xmax", df["ann_bbox_xmin"] + df["ann_bbox_width"])
         df.insert(10, "ann_bbox_ymax", df["ann_bbox_ymin"] + df["ann_bbox_height"])
-    
+
         # debug print(df.info())
-    
+
         # Join the annotions with the information about the image to add the image columns to the dataframe
         df = pd.merge(images, df, left_on="img_id", right_on="ann_image_id", how="left")
-        df = pd.merge(df, categories, left_on="ann_category_id", right_on="cat_id", how="left")
-        
+        df = pd.merge(
+            df, categories, left_on="ann_category_id", right_on="cat_id", how="left"
+        )
+
         # Rename columns if needed from the coco column name to the pylabel column name
-        df.rename(columns={"img_file_name": "img_filename"}, inplace=True)        
+        df.rename(columns={"img_file_name": "img_filename"}, inplace=True)
         df.rename(columns={"img_path": "img_path"}, inplace=True)
         df["img_path"] = add_path
-        
+
         # Drop columns that are not in the schema
         df = df[df.columns.intersection(self.schema)]
-    
+
         # Add missing columns that are in the schema but not part of the table
         df[list(set(self.schema) - set(df.columns))] = ""
-    
+
         # Reorder columns
         df = df[self.schema]
         df.index.name = "id"
         df.annotated = 1
-    
+
         # Fill na values with empty strings which resolved some errors when
         # working with images that don't have any annotations
         df.fillna("", inplace=True)
-    
+
         # These should be strings
         df.cat_id = df.cat_id.astype(str)
-    
+
         # These should be integers
         df.img_width = df.img_width.astype(int)
         df.img_height = df.img_height.astype(int)
-    
+
         dataset = Dataset(df)
-    
+
         # Assign the filename (without extension) as the name of the dataset
-        dataset.name = Path(path).stem    
+        dataset.name = Path(path).stem
         dataset.path_to_annotations = PurePath(path).parent
+
         dataset.path_to_imgs = PurePath(image_dir)
-    
+
         if self.dataset is not None:
             # Append the new dataset to the existing dataset
-            self.dataset.df = pd.concat([self.dataset.df, dataset.df], ignore_index=True)
+            self.dataset.df = pd.concat(
+                [self.dataset.df, dataset.df], ignore_index=True
+            )
         else:
             self.dataset = dataset
-        
+
     def extract_labelme(
         self,
         labelme_folder: str,
         category_list: List = None,
-        train_split_rate: float = 1
+        train_split_rate: float = 1,
     ) -> Coco:
         """
         Args:
@@ -364,10 +371,10 @@ class labelme2yolo:
         ):
             try:
                 data = load_json(json_path)
-                
+
                 # get image size
                 image_path = str(Path(labelme_folder) / data["imagePath"])
-                
+
                 # use the image sizes provided by labelme (they already account for
                 # things such as EXIF orientation)
                 width = data["imageWidth"]
@@ -380,7 +387,7 @@ class labelme2yolo:
                 for shape in data["shapes"]:
                     # set category name and id
                     category_name = shape["label"]
-                    
+
                     category_id = None
                     for (
                         coco_category_id,
@@ -396,7 +403,7 @@ class labelme2yolo:
                             CocoCategory(id=category_id, name=category_name)
                         )
                         category_ind += 1
-    
+
                     # circles and lines to segmentation
                     if shape["shape_type"] == "circle":
                         (cx, cy), (x1, y1) = shape["points"]
@@ -421,7 +428,7 @@ class labelme2yolo:
                             y1 + 1e-3,
                         ]
                         shape["shape_type"] = "polygon"
-    
+
                     # parse bbox/segmentation
                     if shape["shape_type"] == "rectangle":
                         x1 = shape["points"][0][0]
@@ -448,14 +455,14 @@ class labelme2yolo:
                 coco.add_image(coco_image)
             except Exception as e:
                 continue
-        
+
         if 0 < train_split_rate < 1:
             result = coco.split_coco_as_train_val(train_split_rate)
             self.json2df(result["train_coco"].json, labelme_folder, "train")
             self.json2df(result["val_coco"].json, labelme_folder, "val")
         else:
-            self.json2df(coco.json, labelme_folder)                    
-                                
+            self.json2df(coco.json, labelme_folder)
+
     def convert(
         self,
         input_dir: str,
@@ -512,44 +519,48 @@ class labelme2yolo:
             ['training/dataset.yaml', 'training/labels/frame_0002.txt', ...]
 
         """
-        self.extract_labelme(labelme_folder=input_dir, train_split_rate=train_split_rate)
+        self.extract_labelme(
+            labelme_folder=input_dir, train_split_rate=train_split_rate
+        )
         add_path = []
-        ds = self.dataset        
+        ds = self.dataset
 
         # Inspired by https://github.com/aws-samples/groundtruth-object-detection/blob/master/create_annot.py
         yolo_dataset = ds.df.copy(deep=True)
-        
+
         # Convert nan values in the split column from nan to '' because those are easier to work with with when building paths
         yolo_dataset.split = yolo_dataset.split.fillna("")
         for data_path in yolo_dataset["img_path"]:
             if data_path not in add_path:
                 add_path.append(data_path)
-                
+
         # Create all of the paths that will be used to manage the files in this dataset
         path_dict = {}
 
         # The output path is the main path that will be used to create the other relative paths
         path = PurePath(output_dir)
         path_dict["label_path"] = output_dir
-        
+
         # The /images directory should be next to the /labels directory
         path_dict["image_path"] = str(PurePath(path, "images"))
-        
+
         # The root directory is in parent of the /labels and /images directories
         path_dict["root_path"] = str(PurePath(path.parent))
-        
+
         # The YAML file should be in root directory
         path_dict["yaml_path"] = str(PurePath(path, yaml_file))
-        
+
         # The root directory will usually be next to the yolov5 directory.
         # Specify the relative path
         path_dict["root_path_from_yolo_dir"] = str(PurePath("../"))
-        
+
         # If these default values to not match the users environment then they can manually edit the YAML file
         if copy_images:
             # Create the folder that the images will be copied to
             for i in range(len(add_path)):
-                Path(path_dict["image_path"]+add_path[i]).mkdir(parents=True, exist_ok=True)
+                Path(path_dict["image_path"] + add_path[i]).mkdir(
+                    parents=True, exist_ok=True
+                )
 
         # Drop rows that are not annotated
         # Note, having zero annotates can still be considered annotated
@@ -600,18 +611,25 @@ class labelme2yolo:
 
         # Create folders to store annotations
         if output_dir == None:
-            dest_folder = PurePath(ds.path_to_annotations, yolo_dataset.iloc[0].img_folder)
+            dest_folder = PurePath(
+                ds.path_to_annotations, yolo_dataset.iloc[0].img_folder
+            )
         else:
             dest_folder = str(PurePath(output_dir, "labels"))
 
         for i in range(len(add_path)):
-            Path(dest_folder+add_path[i]).mkdir(parents=True, exist_ok=True)
+            Path(dest_folder + add_path[i]).mkdir(parents=True, exist_ok=True)
 
         unique_images = yolo_dataset["img_filename"].unique()
         output_file_paths = []
-        pbar = tqdm(desc="Converting labelme annotations to YOLO format", total=len(unique_images))
+        pbar = tqdm(
+            desc="Converting labelme annotations to YOLO format",
+            total=len(unique_images),
+        )
         for img_filename in unique_images:
-            df_single_img_annots = yolo_dataset.loc[yolo_dataset.img_filename == img_filename]
+            df_single_img_annots = yolo_dataset.loc[
+                yolo_dataset.img_filename == img_filename
+            ]
             basename, _ = os.path.splitext(img_filename)
             annot_txt_file = basename + ".txt"
             # Use the value of the split collumn to create a directory
@@ -619,7 +637,7 @@ class labelme2yolo:
             split_dir = df_single_img_annots["img_path"].iloc[0]
             annot_dir = dest_folder + split_dir
             destination = str(PurePath(annot_dir, annot_txt_file))
-          
+
             # If segmentation = false then output bounding boxes
             if segmentation == False:
                 df_single_img_annots.to_csv(
@@ -644,7 +662,9 @@ class labelme2yolo:
                     # Create one row per row in the data frame
                     for i in range(0, df_single_img_annots.shape[0]):
                         row = str(df_single_img_annots.iloc[i].cat_id)
-                        segmentation_array = df_single_img_annots.iloc[i].ann_segmentation[0]
+                        segmentation_array = df_single_img_annots.iloc[
+                            i
+                        ].ann_segmentation[0]
 
                         # Iterate through every value of the segmentation array
                         # To normalize the coordinates from 0-1
@@ -696,12 +716,11 @@ class labelme2yolo:
             # Build a dict with all of the values that will go into the YAML file
             dict_file = {}
             dict_file["path"] = path_dict["root_path_from_yolo_dir"]
-          
 
             # Define the path for train/val data
             dict_file["train"] = path_dict["image_path"]
             dict_file["val"] = path_dict["image_path"]
-          
+
             for i in range(len(add_path)):
                 if add_path[i] == "/train":
                     dict_file["train"] = str(PurePath(path_dict["image_path"], "train"))
@@ -713,7 +732,9 @@ class labelme2yolo:
 
             # Save the yamlfile
             with open(path_dict["yaml_path"], "w") as file:
-                documents = yaml.dump(dict_file, file, encoding="utf-8", allow_unicode=True)                
+                documents = yaml.dump(
+                    dict_file, file, encoding="utf-8", allow_unicode=True
+                )
                 output_file_paths = [path_dict["yaml_path"]] + output_file_paths
 
         return output_file_paths
